9aeb98f9884e4914b55835da8b80105e5a87551a
CMN <chaalsgmll@gmail.com>
2022-12-29 07:45:03 +0900

    following: 4918c7b improve solving and move file

--- programmers/kakao/_2018_blind_recruitment/_1_secret_map.java
+++ programmers/kakao/_2018_blind_recruitment/_1_secret_map.java
@@ -2,6 +2,17 @@ package src.kakao._2018_blind_recruitment;
 
 import java.util.Arrays;
 
+/*
+ 정확성  테스트
+테스트 1 〉	통과 (0.08ms, 71.7MB)
+테스트 2 〉	통과 (0.08ms, 74.1MB)
+테스트 3 〉	통과 (0.06ms, 77.4MB)
+테스트 4 〉	통과 (0.07ms, 76.1MB)
+테스트 5 〉	통과 (0.07ms, 76.7MB)
+테스트 6 〉	통과 (0.10ms, 75.8MB)
+테스트 7 〉	통과 (0.07ms, 78.1MB)
+테스트 8 〉	통과 (0.05ms, 82.6MB)
+ */
 public class _1_secret_map {
     public static void main(String[] args) {
 //        int n = 5;
@@ -18,15 +29,12 @@ public class _1_secret_map {
         String[] answer = new String[n];
         StringBuilder sb;
         for (int i=0; i<n; i++) {
-            int num1 = arr1[i];
-            int num2 = arr2[i];
-            String binary1 = Integer.toBinaryString(num1);
-            String binary2 = Integer.toBinaryString(num2);
-            binary1 = addZero(binary1, n);
-            binary2 = addZero(binary2, n);
+            int num = arr1[i] | arr2[i];
+            String binary = Integer.toBinaryString(num);
+            binary = addZero(binary, n);
             sb = new StringBuilder();
             for (int j=0; j<n; j++) {
-                if (binary1.charAt(j) == '0' && binary2.charAt(j) == '0') {
+                if (binary.charAt(j) == '0') {
                     sb.append(" ");
                 } else {
                     sb.append("#");
@@ -40,12 +48,12 @@ public class _1_secret_map {
     private static String addZero(String binary, int n) {
         int subtract = n - binary.length();
         StringBuilder sb = new StringBuilder();
-        sb.append(binary);
         if (subtract > 0) {
             while (subtract-- > 0) {
-                sb.insert(0, "0");
+                sb.append("0");
             }
         }
+        sb.append(binary);
         return sb.toString();
     }
 }
