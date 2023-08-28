a07da1db851e0749d2cb2c24d1d51b580d7d9710
CMN <chaalsgmll@gmail.com>
2023-08-29 08:09:08 +0900

    Fix: 76bf93b3041b8c10e424302df4ca74b3c70cd10a change class name adding numbering

--- programmers/basic.training/level0/_1_문자열_출력하기.java
+++ programmers/basic.training/level0/_1_문자열_출력하기.java
@@ -2,7 +2,7 @@ package level0;
 
 import java.util.Scanner;
 
-public class _문자열_출력하기 {
+public class _1_문자열_출력하기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
